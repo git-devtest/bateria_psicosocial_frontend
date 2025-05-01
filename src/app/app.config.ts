8c0d3348caff5ee2c1cd57adf4185adea9091abe
Jhon Harold Hincapie <jhonmaster+git@gmail.com>
2025-05-01 11:01:24 -0500

    Se hace limpieza de dependencias no usadas y se elimina la cache de node_modules y se instalan nuevamente las dependencias

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,12 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
-import { provideLucideIcons } from 'lucide-angular';
-import { Home, Users, Building, KeyRound } from 'lucide-angular/icons';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi()),
-    provideLucideIcons({ Home, Users, Building, KeyRound }) // Importamos solo los íconos que usamos
+    provideHttpClient(withInterceptorsFromDi())
   ]
 };
